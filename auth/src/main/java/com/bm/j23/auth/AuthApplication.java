d32f168cb1afb2efcab25e49c21a3d1648854824
Mostafa ElSheikh <mostafa.elsheikh@vodafone.com>
2023-12-09 14:26:01 +0200

    M2: Add Eureka Server - Assign Eureka Client to Auth Microservice

--- auth/src/main/java/com/bm/j23/auth/AuthApplication.java
+++ auth/src/main/java/com/bm/j23/auth/AuthApplication.java
@@ -3,6 +3,7 @@ package com.bm.j23.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableJpaRepositories("com.bm.j23.auth.model.persistence.repositories")
 @EntityScan("com.bm.j23.auth.model.persistence")
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthApplication {
 
     public static void main(String[] args) {
